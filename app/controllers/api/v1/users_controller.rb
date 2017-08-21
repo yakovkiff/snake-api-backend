455590d4dbfe2d83268f4f4f40158a87c98df592
github username <github email address>
2017-08-21 17:46:54 -0400

    updated UsersAdapter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
+  	user = User.create(user_params)
+  	render json: user
+  end
 
+  private
+  def user_params
+  	params.require(:users).permit(:name, :email)
   end
 
 end

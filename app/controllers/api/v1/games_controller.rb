8f877928830f507b4d68dc3396c42423a609065c
yakovkiff <yakov.kiffel@gmail.com>
2017-12-20 17:11:39 -0500

    save game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,45 +1,46 @@
 class Api::V1::GamesController < ApplicationController
 
   def index
-
-    game = Game.first
+    game = Game.last
     byebug
     render json: { game: game, snake_head: game.snake_head, tail: game.snake_head.tails }
   end
 
   def create_or_update_game
-  	game = Game.find_by(frontend_id: game_params[:id])
-    if game
-      game.update(user_id: game_params[:user][:id], frontend_id: game_params[:id])
-      update_snake_head_and_tail(game)
-    else
-      game = Game.create(user_id: game_params[:user][:id], frontend_id: game_params[:id])
+    byebug
+  	# game = Game.last
+    # if game
+      # game.update(user_id: params[:user][:id])
+      # update_snake_head_and_tail(game)
+    # else
+      # game = Game.create(user_id: params[:user][:id])
+      game = Game.create
       create_snake_head_and_tail(game)
-    end
+    # end
     render json: { game: game, snake_head: game.snake_head, tail: game.tails }
-    # ask an instructor why we can't do game.snake_head.tails
+    # why can't we do game.snake_head.tails
   end
 
 
   private
-  def game_params
-  	params.require(:game).permit({:user => {}}, {:snakeCoordinatesAndBearing => {}}, :id)
-  end
+  # def params
+  # 	params.require(:game).permit({:user => {}}, {:snakeCoordinatesAndBearing => {}}, :id)
+  # end
 
   def create_snake_head_and_tail(game)
     snake_head = SnakeHead.new(game_id: game.id)
-    snake_head.bearing = game_params[:snakeCoordinatesAndBearing][:snake][:bearing]
-    snake_head.x = game_params[:snakeCoordinatesAndBearing][:snake][:coordinates][0]
-    snake_head.y = game_params[:snakeCoordinatesAndBearing][:snake][:coordinates][1]
+    snake_head.bearing = params[:snakeCoordinatesAndBearing][:snake][:bearing]
+    snake_head.x = params[:snakeCoordinatesAndBearing][:snake][:coordinates][0]
+    snake_head.y = params[:snakeCoordinatesAndBearing][:snake][:coordinates][1]
     snake_head.save
     create_tail(snake_head)
   end
 
   def update_snake_head_and_tail(game)
     snake_head = game.snake_head
-    snake_head.bearing = game_params[:snakeCoordinatesAndBearing][:snake][:bearing]
-    snake_head.x = game_params[:snakeCoordinatesAndBearing][:snake][:coordinates][0]
-    snake_head.y = game_params[:snakeCoordinatesAndBearing][:snake][:coordinates][1]
+    snake_head.bearing = params[:snakeCoordinatesAndBearing][:snake][:bearing]
+    snake_head.x = params[:snakeCoordinatesAndBearing][:snake][:coordinates][0]
+    snake_head.y = params[:snakeCoordinatesAndBearing][:snake][:coordinates][1]
     snake_head.save
     update_tail(snake_head)
   end
@@ -47,7 +48,7 @@ class Api::V1::GamesController < ApplicationController
   # {snake: snakeHead.coordinatesAndBearing(), tail: snakeHead.tailCoordinatesAndBearing()}
 
   def create_tail(snake_head)
-    tail_array_of_hashes = game_params[:snakeCoordinatesAndBearing][:tail]
+    tail_array_of_hashes = params[:snakeCoordinatesAndBearing][:tail]
     tail_array_of_hashes.each do |tail_hash|
       byebug
       tail_block = Tail.new(snake_head_id: snake_head.id)
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-	belongs_to :user
+	belongs_to :user, optional: true
 	has_one :snake_head
 	has_many :tails, through: :snake_head
 end
